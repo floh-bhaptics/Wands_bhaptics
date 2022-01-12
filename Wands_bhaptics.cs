dc6dd57e2ac2580a7a4264b4b1ed7757de23bbc5
floh-bhaptics <floh@fahrenbergers.de>
2022-01-12 21:19:14 +0100

    Projektdateien hinzufügen.

---
+++ Wands_bhaptics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace Wands_bhaptics
+{
+    public class Wands_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        /*
+        [HarmonyPatch(typeof(VertigoPlayer), "Die")]
+        public class bhaptics_PlayerDies
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+        */
+    }
+}
