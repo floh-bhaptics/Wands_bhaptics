1d28fb08383a622685e4654734d447b8eb1252e6
floh-bhaptics <floh@fahrenbergers.de>
2022-01-13 08:51:37 +0100

    Spell effect only upon release, getting directional hits

--- Wands_bhaptics.cs
+++ Wands_bhaptics.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using UnityEngine;
 using MelonLoader;
 using HarmonyLib;
 using MyBhapticsTactsuit;
@@ -62,18 +63,26 @@ namespace Wands_bhaptics
             }
         }
 
-        [HarmonyPatch(typeof(Cortopia.Scripts.Player.PlayerControl), "TakeDamage", new Type[] { typeof(Assets.Scripts.Enums.DamageType), typeof(float), typeof(UnityEngine.Vector2) })]
+        [HarmonyPatch(typeof(Cortopia.Scripts.Player.PlayerControl), "TakeDamage", new Type[] { typeof(Assets.Scripts.Enums.DamageType), typeof(float), typeof(Vector2) })]
         public class bhaptics_TakeDamage
         {
             [HarmonyPostfix]
-            public static void Postfix(Assets.Scripts.Enums.DamageType damageType, float damage, UnityEngine.Vector2 hitDirection)
+            public static void Postfix(Cortopia.Scripts.Player.PlayerControl __instance, Assets.Scripts.Enums.DamageType damageType, float damage, Vector2 hitDirection)
             {
-                tactsuitVr.PlaybackHaptics("Impact");
+                // bhaptics starts in the front, then rotates to the left. 0° is front, 90° is left, 270° is right.
+                Vector2 frontFacing = new Vector2(1f, 1f);
+                float playerRotation = __instance.CameraTransform.rotation.eulerAngles.y;
+                float hitAngle = Vector2.SignedAngle(hitDirection, frontFacing);
+                float myAngle = hitAngle - playerRotation;
+                myAngle *= -1f;
+                float correctedAngle = 360f - myAngle;
+                tactsuitVr.LOG("Hit: " + correctedAngle.ToString());
+                tactsuitVr.PlayBackHit("Impact", correctedAngle, 0f);
             }
         }
 
-        [HarmonyPatch(typeof(Cortopia.Scripts.Wand.WandManager), "SpellPress", new Type[] { typeof(Assets.Scripts.Enums.WandHand), typeof(int) })]
-        public class bhaptics_SpellPress
+        [HarmonyPatch(typeof(Cortopia.Scripts.Wand.WandManager), "SpellRelease", new Type[] { typeof(Assets.Scripts.Enums.WandHand), typeof(int) })]
+        public class bhaptics_SpellRelease
         {
             [HarmonyPostfix]
             public static void Postfix(Cortopia.Scripts.Wand.WandManager __instance, Assets.Scripts.Enums.WandHand wandHand, int spellSlotIndex)
@@ -91,20 +100,10 @@ namespace Wands_bhaptics
             public static void Postfix(Cortopia.Scripts.Player.PlayerControl __instance, float changeValue)
             {
                 if (changeValue > 0f) tactsuitVr.PlaybackHaptics("Healing");
-                tactsuitVr.LOG("UpdateHealth: " + __instance.Health.ToString());
-                
-            }
-        }
-
-        [HarmonyPatch(typeof(Cortopia.Scripts.Player.PlayerControl), "ChangeHealth", new Type[] { typeof(float) })]
-        public class bhaptics_ChangeHealth
-        {
-            [HarmonyPostfix]
-            public static void Postfix(Cortopia.Scripts.Player.PlayerControl __instance, float changeValue)
-            {
-                if (changeValue > 0f) tactsuitVr.PlaybackHaptics("Healing");
-                tactsuitVr.LOG("ChangeHealth: " + __instance.Health.ToString());
-
+                //tactsuitVr.LOG("UpdateHealth: " + __instance.Health.ToString());
+                if (__instance.Health == 0f) { tactsuitVr.StopHeartBeat(); return; }
+                if (__instance.Health <= 25f) tactsuitVr.StartHeartBeat();
+                else tactsuitVr.StopHeartBeat();
             }
         }
 
